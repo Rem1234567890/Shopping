cdf3833da0d96546a98b5e6fad823cf985a2c69b
Robert Murray <remurray.2012@mymail.becker.edu>
2018-10-11 09:13:10 -0400

    add seed file

--- db/seeds.rb
+++ db/seeds.rb
@@ -6,14 +6,14 @@
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
 
-# categories = Category.create([
-# 	{name: "Produce"}, 
-# 	{name: "Meat & Seafood"},
-# 	{name: "Deli"}, 
-# 	{name: "Bakery"}, 
-# 	{name: "Frozen"}, 
-# 	{name: "Packaged Foods"}, 
-# 	{name: "Paper Goods"}, 
-# 	{name: "Health and Toiletries"}, 
-# 	{name: "Other"}
-# ])
+categories = Category.create([
+	{name: "Produce"}, 
+	{name: "Meat & Seafood"},
+	{name: "Deli"}, 
+	{name: "Bakery"}, 
+	{name: "Frozen"}, 
+	{name: "Packaged Foods"}, 
+	{name: "Paper Goods"}, 
+	{name: "Health and Toiletries"}, 
+	{name: "Other"}
+])
